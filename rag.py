3d9dd5c302da6bb7018e49b23f5a0fd48b10895a
Sachursm <sachuretnasm@gmail.com>
2026-05-31 15:43:20 +0530

    feat: added feature to read youtube transcripts as url

--- rag.py
+++ rag.py
@@ -12,6 +12,9 @@ from langchain_core.documents import Document
 from playwright.async_api import async_playwright
 import asyncio
 import pypdf
+import re
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import YouTube
 
 load_dotenv()
 
@@ -121,25 +124,57 @@ async def scrape_with_playwright(url: str) -> Document:
         await browser.close()
         return Document(page_content=content, metadata={"source": url})
 
-def scrape_urls(urls:list) -> list:
-  result = []
-  for url in urls:
-    loader = WebBaseLoader(
-        web_paths=[url],
-        header_template={"User-Agent": "Mozilla/5.0"}
-    )
-    data = loader.load()
-    if len(data[0].page_content) < 500:
-      doc = asyncio.run(scrape_with_playwright(url))
-      result.append(doc)
-    else:
-      doc = Document(
-          page_content=data[0].page_content,
-          metadata= data[0].metadata
-      )
-      result.append(doc)
-  return result
+def find_youtube_url(url: str) -> str:
+    if re.search(r"(youtube\.com|youtu\.be)", url):
+        return url
 
+def extract_video_id(url):
+    match = re.search(r"(?:v=|youtu\.be/)([a-zA-Z0-9_-]{11})", url)
+    return match.group(1) if match else None
+
+def get_video_title(url: str) -> str:
+    try:
+        yt = YouTube(url)
+        return yt.title
+    except Exception:
+        return url 
+
+def get_transcript(video_id):
+    try:
+        api = YouTubeTranscriptApi()
+        transcript = api.fetch(video_id)
+        return " ".join([t.text for t in transcript])
+    except Exception as e:
+        raise ValueError(f"Could not get transcript: {e}")
+
+def scrape_urls(urls: list) -> list:
+    result = []
+    for url in urls:
+        if find_youtube_url(url):
+            title = get_video_title(url)
+            video_id = extract_video_id(url)
+            transcript = get_transcript(video_id)
+            result.append(Document(
+                page_content=transcript,
+                metadata={"source": url, "title": title}
+            ))
+        else:
+            loader = WebBaseLoader(
+                web_paths=[url],
+                header_template={"User-Agent": "Mozilla/5.0"}
+            )
+            data = loader.load()
+            if len(data[0].page_content) < 500:
+                doc = asyncio.run(scrape_with_playwright(url))
+                result.append(doc)
+            else:
+                doc = Document(
+                    page_content=data[0].page_content,
+                    metadata=data[0].metadata
+                )
+                result.append(doc)
+    return result
+    
 def extract_pdf(files: list)-> list:
     result = []
     for file in files:
