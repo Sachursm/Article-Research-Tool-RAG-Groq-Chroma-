da53430cce02565c463bef4be7362d93a9477b6c
Sachursm <sachuretnasm@gmail.com>
2026-06-01 18:10:49 +0530

    perf: add cache_resource to get_embeddings for faster multi-user loading

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -3,6 +3,9 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from rag.config import COLLECTION_NAME, VECTORSTORE_DIR, EMBEDDING_MODEL
 
 #embeddings created separately
+import streamlit as st
+
+@st.cache_resource    #to run the model one time for everone in the hugging face
 def get_embeddings():
     return HuggingFaceEmbeddings(
         model_name=EMBEDDING_MODEL,
@@ -14,3 +17,5 @@ def get_vector_store(embeddings, session_id):  # receives embeddings and session
     return Chroma(collection_name = f"{COLLECTION_NAME}_{session_id}",
                   persist_directory = str(VECTORSTORE_DIR),
                   embedding_function=embeddings)
+
+
